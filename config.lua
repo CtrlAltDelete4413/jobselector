c25c01a3d0905d19af936395437a9bbe0cf68eef
CtrlAltDelete4413 <95325881+CtrlAltDelete4413@users.noreply.github.com>
2025-03-08 17:05:20 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,21 +1,11 @@
 Config = {}
 
-Config.Target = "ox" 
-
 Config.JobList = {
-    {label = "Police", value = "police"},
-    {label = "EMS", value = "ambulance"},
-    {label = "Mechanic", value = "mechanic"},
-    {label = "Taxi", value = "taxi"},
-    {label = "Trucker", value = "trucker"},
-    {label = "Security", value = "security"},
-    {label = "Real Estate", value = "realestate"},
-    {label = "Journalist", value = "journalist"},
-    {label = "Lawyer", value = "lawyer"},
-    {label = "Bartender", value = "bartender"}
+    {label = "butcher", value = "butcher"},
+    {label = "bus", value = "bus"}
 }
 
 Config.JobNPC = {
-    model = "s_m_m_highsec_01", -- Model NPC
-    coords = vector4(-267.94, -957.52, 31.22, 205.0) -- Lokasi NPC
+    model = "a_f_y_business_01",
+    coords = vector4(-543.89, -199.00, 38.22, 129.16)
 }
